f0727cab66713d0252a43f3f5a945a98b198c92a
Cesargarciajr <>
2023-12-08 01:49:04 +0000

    updated shopping bag template

--- bag/contexts.py
+++ bag/contexts.py
@@ -5,7 +5,7 @@ from products.models import Product
 
 def bag_contents(request):
 
-    bag_items =[]
+    bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
@@ -21,18 +21,18 @@ def bag_contents(request):
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
-        'product_count': product_count, 
+        'product_count': product_count,
         'delivery': delivery,
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
