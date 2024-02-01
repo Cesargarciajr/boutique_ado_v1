7c59dfd6975505404611ccc6eab1d61a49c45273
Cesargarciajr <>
2024-02-01 02:49:01 +0000

    Product admin - Finished add product and functionality

--- profiles/views.py
+++ profiles/views.py
@@ -16,6 +16,8 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid')
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
