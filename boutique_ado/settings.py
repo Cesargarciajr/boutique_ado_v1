02258c7eb48cfa3080362e57ce7d2d2a88613bbd
Cesar Garcia <cesargarcia.elder@gmail.com>
2024-02-05 01:37:27 +0000

    test send real email

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -229,7 +229,7 @@ DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'cesargarcia.elder@gmail.com'
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
@@ -237,4 +237,4 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_MAIL = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
